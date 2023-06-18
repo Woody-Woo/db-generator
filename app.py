aca625106b4da66cc5ac4c7ad3c7966e057328e8
ivan <ivan@sqlbackupandftp.com>
2023-06-18 22:31:11 +0600

    -

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
+import logging
 from flask import Flask, render_template, request, jsonify
-import subprocess
-
 from config import load_config
 from sql_utils import execute_sql_scripts, split_sql_script,get_databases
 from gpt_utils import transform_input_to_sql, retry_transform_input_to_sql
-import psycopg2
-from psycopg2 import extras
 from flask_httpauth import HTTPBasicAuth
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 auth = HTTPBasicAuth()
 
-# Инициализация Flask приложения
 app = Flask(__name__)
 
 config_data = load_config()
@@ -21,25 +19,22 @@ def verify_password(username, password):
         return username == config_data["SITE_AUTH"]["USERNAME"] and password == config_data["SITE_AUTH"]["PASSWORD"]
     return False
 
-
 @app.route('/', methods=['GET', 'POST'])
 @auth.login_required
 def execute_sql():
     if request.method == 'GET':
         try:
-            # Загрузка данных конфигурации
             default_db_type = 'MYSQL'
             config = load_config()
             databases = get_databases(default_db_type,config[default_db_type])
+            logging.info(f'Successfully got databases for {default_db_type}: {databases}')
             return render_template('index.html', config=config[default_db_type], databases=databases)
         except Exception as e:
+            logging.exception(e)
             return str(e)
     elif request.method == 'POST':
         user_input = request.form.get('user_input', '')
-
-        # Получение данных из формы подключения к базе данных
         db_type = request.form.get('db_type', '')
-
         sql = ""
         config = {
                 "HOST": request.form.get('db_host', ''),
@@ -52,24 +47,25 @@ def execute_sql():
             databases = get_databases(db_type, config)
             sql = transform_input_to_sql(db_type,user_input)
             result = execute_sql_scripts(db_type, config, split_sql_script(db_type,sql))
+            logging.info(f'Successfully executed SQL scripts: {sql}. Result: {result}')
             
             return jsonify({'sql': sql, 'result': result, 'config' : config, 'databases' : databases})
         except Exception as e:
+            logging.exception(e)
             return jsonify({'error': str(e), 'sql': sql})
 
 @app.route('/get_default_values', methods=['POST'])
 def get_default_values():
     db_type = request.form.get('db_type')
-    config = load_config()  # предполагается, что вы имеете функцию для чтения config.json
+    config = load_config()  
     try:
         db_config = config[db_type]
-        try:
-             databases = get_databases(db_type, db_config)
-        except Exception as e:
-            return jsonify(error=str(e))
+        databases = get_databases(db_type, db_config)
+        logging.info(f'Successfully got default values for {db_type}: {db_config}, databases: {databases}')
         
         return jsonify(config=db_config, databases=databases)
     except Exception as e:
+        logging.exception(e)
         return jsonify(error= str(e))
     
 @app.route('/retry', methods=['POST'])
@@ -79,7 +75,6 @@ def retry():
     ai_answer = request.form.get('ai_answer', '')
     db_error = request.form.get('error', '')
 
-    # Получение данных из формы подключения к базе данных
     db_type = request.form.get('db_type', '')
 
     sql = ""
@@ -94,10 +89,13 @@ def retry():
         databases = get_databases(db_type, config)
         sql = retry_transform_input_to_sql(db_type,user_input,ai_answer,db_error)
         result = execute_sql_scripts(db_type, config, split_sql_script(db_type,sql))
+        logging.info(f'Retried execution of SQL scripts: {sql}. Result: {result}')
         
         return jsonify({'sql': sql, 'result': result, 'config' : config, 'databases' : databases})
     except Exception as e:
+        logging.exception(e)
         return jsonify({'error': str(e), 'sql': sql})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- sql_utils.py
+++ sql_utils.py
@@ -2,41 +2,55 @@ import psycopg2
 import psycopg2.extras
 import pymysql
 import pyodbc
+import logging
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def db_connect(db_type, config):
-    if db_type == 'MYSQL':
-        connection = pymysql.connect(host=config["HOST"],
-                                     user=config["USER"],
-                                     password=config["PASSWORD"],
-                                     db=config["DB"],
-                                     charset='utf8mb4',
-                                     cursorclass=pymysql.cursors.DictCursor)
-    elif db_type == 'POSTGRESQL':
-        connection = psycopg2.connect(host=config["HOST"],
-                                      user=config["USER"],
-                                      password=config["PASSWORD"],
-                                      dbname=config["DB"],
-                                      cursor_factory=psycopg2.extras.RealDictCursor)
-    elif db_type == 'SQLSERVER':
-        connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
-                                    f'SERVER={config["HOST"]};'
-                                    f'DATABASE={config["DB"]};'
-                                    f'UID={config["USER"]};'
-                                    f'PWD={config["PASSWORD"]}')
-    else:
-        raise Exception(f"Not implemented for {db_type}")
+    try:
+        if db_type == 'MYSQL':
+            connection = pymysql.connect(host=config["HOST"],
+                                         user=config["USER"],
+                                         password=config["PASSWORD"],
+                                         db=config["DB"],
+                                         charset='utf8mb4',
+                                         cursorclass=pymysql.cursors.DictCursor)
+            logging.info(f'Successfully connected to MYSQL database')
+        elif db_type == 'POSTGRESQL':
+            connection = psycopg2.connect(host=config["HOST"],
+                                          user=config["USER"],
+                                          password=config["PASSWORD"],
+                                          dbname=config["DB"],
+                                          cursor_factory=psycopg2.extras.RealDictCursor)
+            logging.info(f'Successfully connected to POSTGRESQL database')
+        elif db_type == 'SQLSERVER':
+            connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
+                                        f'SERVER={config["HOST"]};'
+                                        f'DATABASE={config["DB"]};'
+                                        f'UID={config["USER"]};'
+                                        f'PWD={config["PASSWORD"]}')
+            logging.info(f'Successfully connected to SQLSERVER database')
+        else:
+            raise Exception(f"Not implemented for {db_type}")
 
-    return connection
+        return connection
+    except Exception as e:
+        logging.exception(e)
+        raise
 
 def get_default_db(db_type):
-    if db_type == 'MYSQL':
-        return 'mysql'
-    elif db_type == 'POSTGRESQL':
-        return 'postgres'
-    elif db_type == 'SQLSERVER':
-        return 'master'
-    else:
-        raise Exception(f"Not implemented for {db_type}")
+    try:
+        if db_type == 'MYSQL':
+            return 'mysql'
+        elif db_type == 'POSTGRESQL':
+            return 'postgres'
+        elif db_type == 'SQLSERVER':
+            return 'master'
+        else:
+            raise Exception(f"Not implemented for {db_type}")
+    except Exception as e:
+        logging.exception(e)
+        raise
 
 def get_databases(db_type, config):
     try:
@@ -50,15 +64,20 @@ def get_databases(db_type, config):
             result = execute_sql_scripts(db_type, config, ["SELECT name FROM sys.databases"])
             return  [x['name'] for x in result[0]] if len(result[0]) > 0 else []
     except Exception as e:
+        logging.exception(e)
         raise Exception("Error while getting databases: " + str(e))
 
 def get_default_db(db_type):
-    if db_type == 'MYSQL':
-        return 'mysql'
-    elif db_type == 'POSTGRESQL':
-        return 'postgres'
-    else:
-        raise Exception(f"Not implemented for {db_type}")
+    try:
+        if db_type == 'MYSQL':
+            return 'mysql'
+        elif db_type == 'POSTGRESQL':
+            return 'postgres'
+        else:
+            raise Exception(f"Not implemented for {db_type}")
+    except Exception as e:
+        logging.exception(e)
+        raise
 
 def execute_sql_scripts(db_type, config, sql_scripts):
     result = []
@@ -74,24 +93,24 @@ def execute_sql_scripts(db_type, config, sql_scripts):
         try:
             for script in sql_scripts:
                 if script.strip() != '':
-                    # Если это команда смены контекста, то пересоздаем подключение
                     if script.strip().startswith("\\c") or script.strip().startswith("USE"):
                         db_name = script.strip().split()[1]
                         new_config = config.copy()
-                        new_config["DB"] = db_name.strip(';')
+                        new_config["DB"] = db_name
                         cursor.close()
                         connection.close()
 
                         connection = db_connect(db_type, new_config)
                         connection.autocommit = True
                         cursor = connection.cursor()
+                        logging.info(f'Successfully switched to database: {db_name}')
                         continue
 
-                    # Проверяем, создается ли новая база данных
                     if script.strip().lower().startswith("create database"):
                         db_name = script.strip().split()[2]
 
                     cursor.execute(script)
+                    logging.info(f'Executed script: {script}')
 
                     if cursor.description:
                         if db_type == 'SQLSERVER':
@@ -110,12 +129,13 @@ def execute_sql_scripts(db_type, config, sql_scripts):
                             result.append(cursor.fetchall())
 
                     connection.commit()
+                    logging.info(f'Successfully committed changes for script: {script}')
 
         finally:
             cursor.close()
     
     except Exception as e:
-        # Если создавалась база данных и произошла ошибка, удаляем ее
+        logging.exception(e)
         if db_name is not None and 'create database' in sql_scripts[0].lower():
             try:
                 drop_db_config = config.copy()
@@ -129,90 +149,105 @@ def execute_sql_scripts(db_type, config, sql_scripts):
                     stripped_db_name = db_name.strip(';')
                     cursor.execute(f'DROP DATABASE {stripped_db_name}')
                     drop_connection.commit()
+                logging.info(f'Successfully dropped the database: {db_name}')
             except Exception as drop_error:
+                logging.exception(drop_error)
                 print(f"Error while trying to drop the database '{db_name}': {str(drop_error)}")
                 pass
-                # raise Exception(f"Error while trying to drop the database '{db_name}': {str(drop_error)}") from drop_error
 
         raise Exception("Error while executing SQL scripts: " + str(e)) from e
 
     finally:
         if connection is not None:
             connection.close()
+            logging.info(f'Successfully closed the database connection')
 
     return result
 
 def split_sql_server_script(script):
-    delimiter = 'GO'
-    lines = script.splitlines()
-    commands = []
-    current_command = []
-    
-    for line in lines:
-        stripped_line = line.strip()
+    try:
+        delimiter = 'GO'
+        lines = script.splitlines()
+        commands = []
+        current_command = []
         
-        if stripped_line.upper() == delimiter:
-            if current_command:
-                commands.append('\n'.join(current_command))
-                current_command = []
-            continue
+        for line in lines:
+            stripped_line = line.strip()
+            
+            if stripped_line.upper() == delimiter:
+                if current_command:
+                    commands.append('\n'.join(current_command))
+                    current_command = []
+                continue
 
-        current_command.append(line)
-    
-    if current_command:
-        commands.append('\n'.join(current_command)) 
-    
-    commands = [cmd for cmd in commands if cmd.strip()]
+            current_command.append(line)
+        
+        if current_command:
+            commands.append('\n'.join(current_command)) 
+        
+        commands = [cmd for cmd in commands if cmd.strip()]
 
-    return commands
+        return commands
+    except Exception as e:
+        logging.exception(e)
+        raise
 
 def split_mysql_and_postgre_script(script):
-    delimiter = ';'
-    lines = script.splitlines()
-    commands = []
-    current_command = []
-    
-    in_dollar_quotes = False
-    
-    for line in lines:
-        if line.strip().lower().startswith('delimiter'):
-            delimiter = line.strip().split(' ')[1]
-            continue
+    try:
+        delimiter = ';'
+        lines = script.splitlines()
+        commands = []
+        current_command = []
+        
+        in_dollar_quotes = False
+        
+        for line in lines:
+            if line.strip().lower().startswith('delimiter'):
+                delimiter = line.strip().split(' ')[1]
+                continue
 
-        if line.strip().startswith('\\c'):
-            if current_command:
+            if line.strip().startswith('\\c'):
+                if current_command:
+                    commands.append('\n'.join(current_command).rstrip(';'))  
+                    current_command = []
+                commands.append(line.strip())
+                continue
+
+            if '$$' in line:
+                in_dollar_quotes = not in_dollar_quotes
+                current_command.append(line)
+                if not in_dollar_quotes:
+                    commands.append('\n'.join(current_command).rstrip(';'))
+                    current_command = []
+                continue
+
+            if not in_dollar_quotes and delimiter in line:
+                sublines = line.split(delimiter)
+                current_command.append(sublines[0])
                 commands.append('\n'.join(current_command).rstrip(';'))  
-                current_command = []
-            commands.append(line.strip())
-            continue
+                current_command = sublines[1:]
+            else:
+                current_command.append(line)
+        
+        if current_command:
+            commands.append('\n'.join(current_command).rstrip(';')) 
+        
+        commands = [cmd for cmd in commands if cmd.strip()]
 
-        if '$$' in line:
-            in_dollar_quotes = not in_dollar_quotes
-            current_command.append(line)
-            if not in_dollar_quotes:
-                commands.append('\n'.join(current_command).rstrip(';'))
-                current_command = []
-            continue
-
-        if not in_dollar_quotes and delimiter in line:
-            sublines = line.split(delimiter)
-            current_command.append(sublines[0])
-            commands.append('\n'.join(current_command).rstrip(';'))  
-            current_command = sublines[1:]
-        else:
-            current_command.append(line)
-    
-    if current_command:
-        commands.append('\n'.join(current_command).rstrip(';')) 
-    
-    commands = [cmd for cmd in commands if cmd.strip()]
-
-    return commands
+        return commands
+    except Exception as e:
+        logging.exception(e)
+        raise
 
 def split_sql_script(db_type, script):
-    if db_type == 'SQLSERVER':
-        return split_sql_server_script(script)
-    elif db_type == 'MYSQL' or db_type == 'POSTGRESQL':
-        return split_mysql_and_postgre_script(script)
-    else:
-        raise Exception(f"Not implemented for {db_type}")
+    try:
+        if db_type == 'SQLSERVER':
+            return split_sql_server_script(script)
+        elif db_type == 'MYSQL' or db_type == 'POSTGRESQL':
+            return split_mysql_and_postgre_script(script)
+        else:
+            raise Exception(f"Not implemented for {db_type}")
+    except Exception as e:
+        logging.exception(e)
+        raise
+
